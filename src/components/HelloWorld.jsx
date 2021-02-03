a1604c475af083022a03bd03cdae314564a36d0c
agustin <agustin.moranr@gmail.com>
2021-02-03 14:24:25 -0600

    [ADD] estructura incial del proyecto

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return <div>hello World</div>;
+};
+
+export default HelloWorld;
---
+++ src/pages/App.js
@@ -0,0 +1,7 @@
+// import "./App.css";
+import HelloWorld from "../components/HelloWorld";
+function App() {
+  return <HelloWorld />;
+}
+
+export default App;
