faa8316f009163c6a7f1c661a4ac476ae353fee3
agustin <agustin.moranr@gmail.com>
2021-03-09 23:16:05 -0600

    [ADD] añadiendo funcionalidad para exapandir más resultados en la seccion de moreLikeThis del modal

--- src/components/ItemModalMoreLikeThis.jsx
+++ src/components/ItemModalMoreLikeThis.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MoreLikeThisCard from './MoreLikeThisCard';
 import ModalSectionTitle from './ModalSectionTitle';
-
+import RoundedButton from './RoundedButton';
 const ItemModalMoreLikeThis = ({ movies }) => {
+	const [moreLikeThis, setMoreLikeThis] = useState([]);
+	const [expandMoreActive, setExpandMoreActive] = useState(false);
+	// console.log(moreLikeThis);
+
+	useEffect(() => {
+		if (expandMoreActive) return setMoreLikeThis(movies);
+		setMoreLikeThis(movies.slice(0, 9));
+	}, [expandMoreActive]);
 	return (
 		<section className='moreLikeThis'>
 			<ModalSectionTitle title='Más títulos similares a este' />
 			<section className='moreLikeThis-wrapper'>
-				{movies.map((movie) => (
+				{moreLikeThis.map((movie) => (
 					<MoreLikeThisCard key={movie.id} movie={movie} />
 				))}
 			</section>
+			<div className={`section-divider ${expandMoreActive ? '' : 'collapsed'}`}>
+				<RoundedButton
+					action={() => setExpandMoreActive(!expandMoreActive)}
+					icon={expandMoreActive ? 'expand_less' : 'expand_more'}
+					className='section-divider-button'
+				/>
+			</div>
 		</section>
 	);
 };
